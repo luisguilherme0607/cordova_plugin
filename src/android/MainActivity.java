c0f51b3e2277d9f8da46a58288cc0f56cd6ae9aa
luisguilherme0607 <luis.g@bool.pt>
2018-12-20 15:53:01 -0100

    imports

--- src/android/MainActivity.java
+++ src/android/MainActivity.java
@@ -18,9 +18,11 @@ import android.hardware.Camera;
 import android.hardware.Camera.CameraInfo;
 
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.content.Intent;
+import android.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
